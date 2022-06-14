bb37b77dd7f56dbe58b266a6abe422a644fcf8d1
unknown <s20867@pjwstk.edu.pl>
2022-06-14 12:15:16 +0200

    initial commit

---
+++ Services/IRepoService.cs
@@ -0,0 +1,12 @@
+using Kolokwium2.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolokwium2.Services
+{
+    public interface IRepoService
+    {
+        
+    }
+}
---
+++ Services/RepoService.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Kolokwium2.Models;
+
+namespace Kolokwium2.Services
+{
+    public class RepoService : IRepoService
+    {
+        // private readonly RepositoryContext _repository;
+        // public RepoService(RepositoryContext repository)
+        // {
+        //     _repository = repository;
+        // }
+    }
+}
